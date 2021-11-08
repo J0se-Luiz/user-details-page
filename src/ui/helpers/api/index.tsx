1250c338c425c45c33903ad39278f57fa9a5520e
Jose Luiz dos Santos Xavier <jlxavier@rd.com.br>
2021-11-08 08:28:03 -0300

    user-details-page

--- src/ui/helpers/api/index.tsx
+++ src/ui/helpers/api/index.tsx
@@ -17,47 +17,56 @@ export const userLogin = (
 
     request(options, function (error: string | undefined, response: { body: any; }) {
 
-        if (error) throw new Error(error);
-
-        if (response.body) {
-            const login = JSON.parse(response.body)
-            const requestListUsers = require('request');
-            const optionsuser = {
-                'method': 'GET',
-                'url': 'https://challenge-fielo.herokuapp.com/users',
-                'headers': {
-                    'x-access-token': login?.token
-                }
-            };
-
-            requestListUsers(optionsuser, function (erroruser: string | undefined, responseListUsers: { body: any; }) {
-                if (erroruser) throw new Error(erroruser);
-
-                if (responseListUsers.body) {
-                    const dataListUsers = JSON.parse(responseListUsers.body)
-
-                    dataListUsers.sort((a: { balance: { points: number; }; }, b: { balance: { points: number; }; }) => {
-                        if (a.balance.points > b.balance.points) {
-                            return -1
-                        } else {
-                            return true
-                        }
-                    })
-                    //  console.log("dentro da api....",dataListUsers); // console.log
-                    setDataLogin({ ...dataLogin, login: login, dataListUsers: dataListUsers, loading: false });
+        // if (error) throw new Error(error);
+        try {
+            if (response.body) {
+                const login = JSON.parse(response.body)
+                const requestListUsers = require('request');
+                const optionsuser = {
+                    'method': 'GET',
+                    'url': 'https://challenge-fielo.herokuapp.com/users',
+                    'headers': {
+                        'x-access-token': login?.token
+                    }
                 };
-            });
-        };
+
+                requestListUsers(optionsuser, function (erroruser: string | undefined, responseListUsers: { body: any; }) {
+                    // if (erroruser) throw new Error(erroruser);
+                    try {
+
+                        if (responseListUsers.body) {
+                            const dataListUsers = JSON.parse(responseListUsers.body)
+
+                            dataListUsers.sort((a: { balance: { points: number; }; }, b: { balance: { points: number; }; }) => {
+                                if (a.balance.points > b.balance.points) {
+                                    return -1
+                                } else {
+                                    return true
+                                }
+                            })
+                            //  console.log("lista dos usuarios....",dataListUsers); // console.log
+                            setDataLogin({ ...dataLogin, login: login, dataListUsers: dataListUsers, loading: false });
+                        };
+                    } catch {
+                        alert(`Failed trying to list users... >>> =..( <<< something wrong is not right hehehe, try again later. ${error}`);
+                        setDataLogin({ ...dataLogin, loading: false });
+                    }
+                });
+            };
+        } catch {
+            alert(`Sign In failed...  >>> =..( <<< something wrong is not right hehehe, try again later. ${error}`);
+            setDataLogin({ ...dataLogin, loading: false });
+        }
     });
 };
 
 export const getInforUser = (token: any, idUser: any, dataUserInfor: any,
     setDataUserInfor: (arg0: any) => void) => {
 
-        setDataUserInfor({ ...dataUserInfor,  loading: true });
+    setDataUserInfor({ ...dataUserInfor, loading: true });
 
     if (dataUserInfor?.userInfor?.user?.id && idUser === dataUserInfor.userInfor.user.id) {
-        setDataUserInfor({ ...dataUserInfor , userInfor: undefined});
+        setDataUserInfor({ ...dataUserInfor, userInfor: undefined });
         return
     }
 
@@ -71,82 +80,87 @@ export const getInforUser = (token: any, idUser: any, dataUserInfor: any,
         }
     };
     requestPerfil(optionsPerfil, function (error: string | undefined, response: { body: any; }) {
-        if (error) throw new Error(error);
+        // if (error) throw new Error(error);
+        try {
 
-        if (response.body) {
-            const perfil = JSON.parse(response.body);
-            // console.log("....... perfil", perfil)
-            // setDataUserInfor({ ...dataUserInfor, user: perfil })
+            if (response.body) {
+                const perfil = JSON.parse(response.body);
+                // console.log("....... perfil", perfil)
+                // setDataUserInfor({ ...dataUserInfor, user: perfil })
 
-            //   retorna  o programa do user
-            const requestProgram = require('request');
-            const optionsProgram = {
-                'method': 'GET',
-                'url': `https://challenge-fielo.herokuapp.com/programs/${perfil.programId}`,
-                'headers': {
-                    'x-access-token': token
-                }
-            };
-
-            requestProgram(optionsProgram, function (error: string | undefined, response: { body: any; }) {
-                if (error) throw new Error(error);
-
-                if (response.body) {
-                    const program = JSON.parse(response.body);
-                    // console.log("..........program", program)
-                    // setDataUserInfor({ ...dataUserInfor, user: perfil, userProgram: program })
-
-                    // retorna o nivel do user
-                    const requestLevelUser = require('request');
-                    const optionsLevelUser = {
-                        'method': 'GET',
-                        'url': `https://challenge-fielo.herokuapp.com/levels/${perfil.levelId}`,
-                        'headers': {
-                            'x-access-token': token
-                        }
-                    };
-
-                    requestLevelUser(optionsLevelUser, function (error: string | undefined, response: { body: any; }) {
-                        if (error) throw new Error(error);
-
-                        if (response.body) {
-                            const userLevel = JSON.parse(response.body);
-                            // console.log("......A API TA RETORNANDO UMA ORDEM ERRADA. userLevel", userLevel)
-                            // setDataUserInfor({ ...dataUserInfor, user: perfil, userProgram: program, userLevel: userLevel })
-
-
-                            // // retorna  as activities
-                            var requesta = require('request');
-                            var optionsa = {
-                                'method': 'GET',
-                                'url': `https://challenge-fielo.herokuapp.com/users/${perfil.id}/activities`,
-                                'headers': {
-                                    'x-access-token': token
-                                }
-                            };
-                            // console.log("................ID QUE TA PASSANDO PARA A API DE ACTIVITIES, A API TA RETORNANDO SEMPRE AS MESMAS 7 ACTIVIES...........", perfil.id)
-
-                            requesta(optionsa, function (error: string | undefined, response: { body: any; }) {
-                                if (error) throw new Error(error);
-                                if (response.body) {
-                                    const activities = JSON.parse(response.body);
-
-                                    activities.sort((a: { date: number; }, b: { date: number; }) => {
-                                        if (a.date > b.date) {
-                                            return -1
-                                        } else {
-                                            return true
-                                        }
-                                    })
-                                    //  console.log("........activities...", activities);
-
-                                    setDataUserInfor({ ...dataUserInfor, userInfor: { user: perfil, userProgram: program, userLevel: userLevel, userActivities: activities}, loading: false })
-                                }
-                            }); // <<<< retorna  as activities 
-                        };
-                    });  // <<<<retorna o nivel do user
+                //   retorna  o programa do user
+                const requestProgram = require('request');
+                const optionsProgram = {
+                    'method': 'GET',
+                    'url': `https://challenge-fielo.herokuapp.com/programs/${perfil.programId}`,
+                    'headers': {
+                        'x-access-token': token
+                    }
                 };
-            });  //   <<< retorna o program 
-        };
+
+                requestProgram(optionsProgram, function (error: string | undefined, response: { body: any; }) {
+                    if (error) throw new Error(error);
+
+                    if (response.body) {
+                        const program = JSON.parse(response.body);
+                        // console.log("..........program", program)
+                        // setDataUserInfor({ ...dataUserInfor, user: perfil, userProgram: program })
+
+                        // retorna o nivel do user
+                        const requestLevelUser = require('request');
+                        const optionsLevelUser = {
+                            'method': 'GET',
+                            'url': `https://challenge-fielo.herokuapp.com/levels/${perfil.levelId}`,
+                            'headers': {
+                                'x-access-token': token
+                            }
+                        };
+
+                        requestLevelUser(optionsLevelUser, function (error: string | undefined, response: { body: any; }) {
+                            if (error) throw new Error(error);
+
+                            if (response.body) {
+                                const userLevel = JSON.parse(response.body);
+                                // console.log("......A API TA RETORNANDO UMA ORDEM ERRADA. userLevel", userLevel)
+                                // setDataUserInfor({ ...dataUserInfor, user: perfil, userProgram: program, userLevel: userLevel })
+
+
+                                // // retorna  as activities
+                                var requesta = require('request');
+                                var optionsa = {
+                                    'method': 'GET',
+                                    'url': `https://challenge-fielo.herokuapp.com/users/${perfil.id}/activities`,
+                                    'headers': {
+                                        'x-access-token': token
+                                    }
+                                };
+                                // console.log("................ID QUE TA PASSANDO PARA A API DE ACTIVITIES, A API TA RETORNANDO SEMPRE AS MESMAS 7 ACTIVIES...........", perfil.id)
+
+                                requesta(optionsa, function (error: string | undefined, response: { body: any; }) {
+                                    if (error) throw new Error(error);
+                                    if (response.body) {
+                                        const activities = JSON.parse(response.body);
+
+                                        activities.sort((a: { date: number; }, b: { date: number; }) => {
+                                            if (a.date > b.date) {
+                                                return -1
+                                            } else {
+                                                return true
+                                            }
+                                        })
+                                        //  console.log("........activities...", activities);
+
+                                        setDataUserInfor({ ...dataUserInfor, userInfor: { user: perfil, userProgram: program, userLevel: userLevel, userActivities: activities }, loading: false })
+                                    }
+                                }); // <<<< retorna  as activities 
+                            };
+                        });  // <<<<retorna o nivel do user
+                    };
+                });  //   <<< retorna o program 
+            };
+        } catch {
+            alert(`failed while trying to fetch user information... >>> =..( <<< something wrong is not right hehehe, try again later. ${error}`);
+            setDataUserInfor({ ...dataUserInfor, loading: false });
+        }
     }); // <<<  retorna o perfil
 }
